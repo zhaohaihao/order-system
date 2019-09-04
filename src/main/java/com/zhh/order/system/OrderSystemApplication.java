b2349e6f7784f201c84a8643d0f85903a14b626b
zhaohaihao <zhh2933@gmail.com>
2019-09-04 23:01:32 +0800

    :art: 新增日志

---
+++ src/main/java/com/zhh/order/system/OrderSystemApplication.java
@@ -0,0 +1,17 @@
+package com.zhh.order.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhh
+ * @description 启动项
+ * @date 2019-08-29 00:34
+ */
+@SpringBootApplication
+public class OrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSystemApplication.class, args);
+    }
+}
